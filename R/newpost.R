73874b64fd56eedde647f1901dc9fc065c8c79df
Sharon Machlis <smachlis@gmail.com>
2023-02-23 20:30:31 -0500

    Added vignette with code snippet suggestion

--- R/newpost.R
+++ R/newpost.R
@@ -2,13 +2,16 @@
 
 #' Create new folder and index.qmd in your blog's posts directory
 #'
-#' @param character string with planned post headline
+#' @param character string with planned post headline. Required
+#' @param description string with post description. Optional but recommended.
+#' @param author string with author name. Suggest including this in a code snippet, see vignette. OPtional.
+#' @param categories string in the format "category1, category2" as a single comma-separated string and NOT a vector.
 #' @param root_dir string your blog's root directory, defaults to here::here()
 #'
 #' @return Creates a new folder in posts directory with yyy--mm-dd-headline-in-lowercase where yyyy-mm-dd is current system date
 #' @export
 #'
-newpost <- function(headline, root_dir = here::here()){
+newpost <- function(headline, description = "", author = "", categories = "", root_dir = here::here()){
   slug <- tolower(headline)
   slug <- gsub('[[:punct:] ]+',' ', slug)
   slug <- trimws(slug)
@@ -18,10 +21,10 @@ fs::dir_create(folder_name)
 setwd(folder_name)
   file_contents <- stringr::str_glue('---
 title: "{headline}"
-author: ""
-description: ""
+author: "{author}"
+description: "{description}"
 date: "{Sys.Date()}"
-categories: []
+categories: [{categories}]
 ---
 ')
 cat(file_contents, file = 'index.qmd')
