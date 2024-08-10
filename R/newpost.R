ceb76abdd030a577841e59278de7ae3843c6b282
Sharon Machlis <smachlis@gmail.com>
2024-08-10 09:54:57 -0400

    Added date as an argument to the function

--- R/newpost.R
+++ R/newpost.R
@@ -7,23 +7,24 @@
 #' @param author string with author name. Suggest including this in a code snippet, see vignette. OPtional.
 #' @param categories string in the format "category1, category2" as a single comma-separated string and NOT a vector.
 #' @param root_dir string your blog's root directory, defaults to here::here()
+#' @param the_date date or string in yyyy-mm-dd format
 #'
 #' @return Creates a new folder in posts directory with yyy--mm-dd-headline-in-lowercase where yyyy-mm-dd is current system date
 #' @export
 #'
-newpost <- function(headline, description = "", author = "", categories = "", root_dir = here::here()){
+newpost <- function(headline, description = "", author = "", categories = "", root_dir = here::here(), the_date = Sys.Date()){
   slug <- tolower(headline)
   slug <- gsub('[[:punct:] ]+',' ', slug)
   slug <- trimws(slug)
   slug <- gsub(" ", "-", slug)
-  folder_name <- paste0(root_dir, "/posts/", Sys.Date(), "-", slug)
+  folder_name <- paste0(root_dir, "/posts/", the_date, "-", slug)
 fs::dir_create(folder_name)
 setwd(folder_name)
   file_contents <- stringr::str_glue('---
 title: "{headline}"
 author: "{author}"
 description: "{description}"
-date: "{Sys.Date()}"
+date: "{the_date}"
 categories: [{categories}]
 ---
 ')
